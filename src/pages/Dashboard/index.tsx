c129d50f793bff87f3ad696a9d589aa9c451ce41
Filipe Alves Cavalcante <filipe.alvescavalcante@gmail.com>
2020-09-05 01:11:20 -0300

    first page Done

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,57 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+import logoImg from '../../assets/Logo.svg';
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <h1>Dashboard</h1>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore Repositórios no Github</Title>
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button>Pesquisar</button>
+      </Form>
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/39503968?s=460&u=3900177c59c7f2c6f3f7571b83b38a83703c278f&v=4"
+            alt="Filipe Alves"
+          />
+          <div>
+            <strong>Desenvolvedor Web</strong>
+            <p>Em busca de conhecimento</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/39503968?s=460&u=3900177c59c7f2c6f3f7571b83b38a83703c278f&v=4"
+            alt="Filipe Alves"
+          />
+          <div>
+            <strong>Desenvolvedor Web</strong>
+            <p>Em busca de conhecimento</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/39503968?s=460&u=3900177c59c7f2c6f3f7571b83b38a83703c278f&v=4"
+            alt="Filipe Alves"
+          />
+          <div>
+            <strong>Desenvolvedor Web</strong>
+            <p>Em busca de conhecimento</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
